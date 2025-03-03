742d7ded3984ba98302aaa31772a9c60b60ab109
gonrogon <gonrogon@gmail.com>
2025-03-03 12:44:49 +0100

    feat: Export all renderers in index.js
    This commit exports all renderers in the index.js file to make them
    available as a module.

---
+++ src/renderers/index.js
@@ -0,0 +1,5 @@
+export { LibraryRenderer } from "./LibraryRenderer.js";
+export { ProjectRenderer } from "./ProjectRenderer.js";
+export { NoteRenderer } from "./NoteRenderer.js";
+export { TextBlockRenderer } from "./TextBlockRenderer.js";
+export { ChecklistRenderer } from "./ChecklistRenderer.js";
