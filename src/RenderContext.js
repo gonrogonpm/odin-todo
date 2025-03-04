1c1733d840504690fa2f18c42aad8f388a73fd95
gonrogon <gonrogon@gmail.com>
2025-03-04 17:08:42 +0100

    Fix: Fix an error in RenderContext

--- src/RenderContext.js
+++ src/RenderContext.js
@@ -77,7 +77,7 @@ export class RenderContext {
      * @returns Value of the parameter.
      */
     getSettingsParam(param, def = null) {
-        if (!this.hasSettings || !(def in this.settings)) {
+        if (!this.hasSettings || !(param in this.settings)) {
             return def;
         }
         
