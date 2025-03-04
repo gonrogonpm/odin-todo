116a7da039b3f15727cee5927a8089322eb98310
gonrogon <gonrogon@gmail.com>
2025-03-04 17:30:43 +0100

    refactor: Update index.js test data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { style } from "./style.css";
 import { App } from "./App.js";
 import { Priority } from "./Priority.js";
 import { Project } from "./Project.js";
@@ -6,17 +7,17 @@ import * as Contents from "./contents/index.js";
 
 const app = new App();
 
-app.library.add(new Project("Test Project"));
-app.library.add(new Project("Mega Project"));
+app.library.addProject(new Project("Test Project"));
+app.library.addProject(new Project("Mega Project"));
 
-app.library.get(0).add(new Note({
+app.library.getProject(0).add(new Note({
     title:       "Test ToDo",
     description: "A generic description",
     dueDate:     Date.now(),
     priority:    Priority.Medium
 }));
 
-const note = app.library.get(0).get(0);
+const note = app.library.getProject(0).get(0);
 note.add(new Contents.TextBlock("A text block for the ToDo"));
 note.add(new Contents.TextBlock("Another text block"));
 note.add(new Contents.Checklist([
@@ -25,11 +26,19 @@ note.add(new Contents.Checklist([
     "Do something more"
 ]));
 
-app.library.get(0).add(new Note({
+app.library.getProject(0).add(new Note({
     title: "Project Foo",
     description: "A great foo project for the web",
     dueDate: null,
     priority: Priority.Low
 }));
 
+app.library.getProject(1).add(new Note({
+    title: "Awesome Note",
+    description: "This note is amazing",
+    dueDate: null,
+    priority: Priority.Critical
+}));
+
 app.render();
+app.setup();
