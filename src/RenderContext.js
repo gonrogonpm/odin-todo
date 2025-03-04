ab3e77a3ddee8eb6b8941f6ee3f8c9a59bd710c4
gonrogon <gonrogon@gmail.com>
2025-03-04 16:54:47 +0100

    refactor: Refactor RenderContext for robustness
    This commit refactors the RenderContext class to improve its robustness
    and usability.
    
    Key features:
    - Adds input validation to `setWrapper` and `setSettings` methods to
      prevent errors with invalid input types.
    - Introduces `getSettingsParam` method for simplified and safer access
      to settings parameters with default values.
    - Updates constructor to use the setter methods for wrapper and settings
      initialization.
    
    This refactor enhances the RenderContext class, making it more reliable
    and easier to use throughout the rendering system.

--- src/RenderContext.js
+++ src/RenderContext.js
@@ -1,19 +1,98 @@
+/**
+ * Defines a class to handle all the paremeters about the rendering context of an object.
+ */
 export class RenderContext {
-    constructor(wrapper, settings = null) {
-        this.wrapper  = wrapper;
-        this.settings = settings;
+    /**
+     * Wrapper.
+     */
+    #wrapper = null;
+
+    /**
+     * Object with the settings.
+     */
+    #settings = null;
+
+    /**
+     * Constructor.
+     * 
+     * @param {object} wrapper Wrapper.
+     * @param {object} settings Settings.
+     */
+    constructor(wrapper = null, settings = null) {
+        this.setWrapper(wrapper);
+        this.setSetting(settings);
     }
 
-    get hasWrapper() {
-        return this.wrapper !== undefined && this.wrapper !== null;
+    /**
+     * Flag indicating whether the context has a wrapper or not.
+     */
+    get hasWrapper() { return this.wrapper != null; }
+
+    /**
+     * Gets the wrapper object.
+     */
+    get wrapper() { return this.#wrapper; }
+
+    /**
+     * Flag indicating whether the context has a settings object or not.
+     */
+    get hasSettings() { return this.settings != null && typeof this.settings === "object"; }
+
+    /**
+     * Gets the settings object.
+     */
+    get settings() { return this.#settings; }
+
+    /**
+     * Sets the wrapper.
+     * 
+     * @param {object} wrapper Wrapper object to set.
+     */
+    setWrapper(wrapper) {
+        if (typeof wrapper !== "object") {
+            throw TypeError("Wrapper must be an object or null");
+        }
+
+        this.#wrapper = wrapper;
     }
 
-    get hasSettings() {
-        return this.settings !== undefined && this.settings !== null;
+    /**
+     * Sets the settings.
+     * 
+     * @param {object} settings Settings object to set.
+     */
+    setSetting(settings) {
+        if (typeof settings !== "object") {
+            throw TypeError("Settings must be an object or null");
+        }
+
+        this.#settings = settings;
     }
 
+    /**
+     * Gets a parameter from the settings object.
+     * 
+     * @param {string} param Parameter to get.
+     * @param {*} def Default value to return if the parameter is not found.
+     * @returns Value of the parameter.
+     */
+    getSettingParam(param, def = null) {
+        if (!this.hasSettings() || !(def in this.settings)) {
+            return def;
+        }
+        
+        return this.#settings[param];
+    }
+
+    /**
+     * Clones the context with new settings.
+     * 
+     * Creates a shallow copy of the context appliying a new settings object.
+     * 
+     * @param {object} settings Settings to set.
+     * @returns Clone of the context with the new settings.
+     */
     cloneWithNewSettings(settings) {
-        let context = new RenderContext(this.wrapper, settings ?? null);
-        return context;
+        return new RenderContext(this.#wrapper, settings);
     }
 }
