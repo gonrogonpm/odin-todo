b511ecff1f26df1470e13e2ba0dfcbb8467c623d
gonrogon <gonrogon@gmail.com>
2025-03-22 17:11:35 +0100

    feat: Add a yes/no question dialog

--- src/renderers/templates/Dialog.js
+++ src/renderers/templates/Dialog.js
@@ -1,9 +1,36 @@
 import * as Common from "./Common.js";
 
+export function QuestionDialog(settings = {}) {
+    const frag   = document.createDocumentFragment();
+    const dialog = document.createElement("dialog");
+    const form   = DialogControls(settings);
+
+    settings.class = [].concat(settings.class ?? [], "question-dialog");
+    Common.SetCommonSettings(dialog, settings);
+
+    if ("title" in settings) {
+        const title = Common.H2({ text: settings.title });
+        dialog.append(title);
+    }
+
+    if ("text" in settings) {
+        const text = Common.P({ text: settings.text });
+        dialog.append(text);
+    }
+
+    if ("open" in settings && settings.open) {
+        dialog.open = true;
+    }
+
+    dialog.append(form);
+    frag.append(dialog);
+    return frag;
+}
+
 export function DeleteDialog(settings = {}) {
     const frag   = document.createDocumentFragment();
     const dialog = document.createElement("dialog");
-    const form   = DeleteDialogForm(settings);
+    const form   = DialogControls(settings);
 
     settings.class = [].concat(settings.class ?? [], "delete-dialog");
     Common.SetCommonSettings(dialog, settings);
@@ -33,7 +60,7 @@ export function DeleteDialog(settings = {}) {
     return frag;
 }
 
-export function DeleteDialogForm(settings = {}) {
+export function DialogControls(settings = {}) {
     const frag     = document.createDocumentFragment();
     const controls = Common.Div({ class: "controls" });
     const confirm  = Common.ConfirmButton();
